52076b42e984a173cb795aeb86598c140d24f336
satokora <satokorambxl@gmail.com>
2020-06-21 18:09:56 -0700

    Updates

---
+++ precache-manifest.ec4a4d5e5c69e9e4368a1b71b05af204.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b415f90265ec96a5358286e7cbb3c32",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c6d7750be6ba0b9ca6a7",
+    "url": "/my-app/static/css/main.1635e94e.chunk.css"
+  },
+  {
+    "revision": "1b5068357a67ee61e361",
+    "url": "/my-app/static/js/2.47aa008d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.47aa008d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d7750be6ba0b9ca6a7",
+    "url": "/my-app/static/js/main.d738d6fa.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "035517c5efb6df411cec8203ae433b57",
+    "url": "/my-app/static/media/11066361.035517c5.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "809c70971a128c9259ffc05c6ca5178b",
+    "url": "/my-app/static/media/marble-surface-2341290.809c7097.jpg"
+  }
+]);
