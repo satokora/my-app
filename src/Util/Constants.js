1c722aa4cc66752fe16d18541becbbc1b0be7c2a
satokora <satokorambxl@gmail.com>
2021-03-04 20:49:55 -0800

    removed constants.js

--- src/Util/Constants.js
+++
@@ -1,4 +0,0 @@
-export const API='https://api.github.com/users';
-export const GITHUB_GRAPHQL_API = 'https://api.github.com/graphql';
-export const USER_NAME='satokora';
-export const AUTHORIZATION_KEY='Bearer TEST';
